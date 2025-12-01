c603214456a7daa1382434b2b18e51514e0026e5
rtocastro <ricktorres@live.com>
2025-11-30 23:52:51 -0800

    added code from render to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
 import './App.css'
 
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 import MainPage from './pages/MainPage'
 
 function App() {
