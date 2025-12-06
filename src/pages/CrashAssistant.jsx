eee78cf85f091f382e1158742ce4f2646b8e049b
rtocastro <ricktorres@live.com>
2025-12-06 09:50:50 -0800

    imported component to page for crashAI

--- src/pages/CrashAssistant.jsx
+++ src/pages/CrashAssistant.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import CrashAIAssistant from "../components/CrashAIAssistant"
 
 export default function CrashAssistant() {
   return (
     <>
-  
+  <CrashAIAssistant />
     </>
   )
 }
